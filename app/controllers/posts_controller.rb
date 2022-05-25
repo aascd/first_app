ed154314717dee6b292ec434fcc97d36015b47aa
aacbc <namazu929@gmail.com>
2022-05-25 14:20:38 +0900

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
- class PostsController < ApplicationController
+class PostsController < ApplicationController
+
   def index
+    @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+    
   end
+
 end
+
